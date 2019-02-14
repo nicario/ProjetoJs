8e6ce58394e3e9b72241692af258c96a3016a771
Nicario Laube de Souza Oliveira <nicarioster@gmail.com>
2019-02-14 15:49:13 -0200

    Adicionando novos dados

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,33 +1,74 @@
+var pacientes = document.querySelectorAll(".paciente");
+
+for(var i = 0; i < pacientes.length; i++) {
+	var paciente = pacientes[i];
+
+	var tdPeso = paciente.querySelector(".info-peso");
+	var peso = tdPeso.textContent;
+
+	var tdAltura = paciente.querySelector(".info-altura");
+	var altura = tdAltura.textContent;
+
+	var tdImc = paciente.querySelector(".info-imc");
+
+	var pesoEhValido = true;
+	var alturaEhValida = true;
+
+	if (peso <= 0 || peso >= 1000) {
+		console.log("Peso Inválido!");
+		pesoEhValido = false;
+		tdImc.textContent = "Peso Inválido";
+		paciente.classList.add(".paciente-invalido");
+	}
+	if (altura <= 0 || altura >= 3.00) {
+		console.log("Altura Inválida!");
+		alturaEhValida = false;
+		tdImc.textContent = "Altura Inválida";
+		paciente.classList.add(".paciente-invalido");
+	}
+	if (alturaEhValida && pesoEhValido) {
+		var imc = peso / (altura * altura);
+		tdImc.textContent = imc.toFixed(2);
+	}
+}
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event){
+event.preventDefault();
+
+var form = document.querySelector("#form-adiciona");
+
+var nome = form.nome.value;
+var peso = form.peso.value;
+var altura = form.altura.value;
+var gordura = form.gordura.value;
+
+var pacienteTr = document.createElement("tr");
+
+var nomeTd = document.createElement("td");
+var pesoTd = document.createElement("td");
+var alturaTd = document.createElement("td");
+var gorduraTd = document.createElement("td");
+var imcTd = document.createElement("td");
+
+nomeTd.textContent = nome;
+pesoTd.textContent = peso;
+alturaTd.textContent = altura;
+gorduraTd.textContent = gordura;
+
+pacienteTr.appendChild(nomeTd);
+pacienteTr.appendChild(pesoTd);
+pacienteTr.appendChild(alturaTd);
+pacienteTr.appendChild(gorduraTd);
+
+var tabela = document.querySelector("#tabela-pacientes");
+
+tabela.appendChild(pacienteTr);
+});
+
+
 var titulo = document.querySelector(".titulo");
-titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
-
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
-
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
-
-var tdImc = paciente.querySelector(".info-imc");
-
-var pesoEhValido = true;
-var alturaEhValida = true;
-
-if(peso <= 0 || peso >= 1000) {
-	console.log("Peso Inválido!");
-	pesoEhValido = false;
-	tdImc.textContent = "Peso Inválido!";
-}
-if (altura <= 0 || altura >= 3.00) {
-	console.log("Altura Inválido!");
-	alturaEhValida = false;
-	tdImc.textContent = "Altura Inválido!";
-}
-if (alturaEhValida && pesoEhValido) {
-	var imc = peso / (altura*altura);
-	tdImc.textContent = imc;
-}
-var imc = peso / (altura * altura); // 100 / 2.0 X 2.0  = 100 / 4 =>>>> 25
-
-tdImc.textContent = imc;
+titulo.addEventListener("click", function() {
+	console.log("Você clicou");
+});
