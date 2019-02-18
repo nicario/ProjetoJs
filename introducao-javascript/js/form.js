492451bad22379013a1f91f4e4725a39aea09059
Nicario Laube de Souza Oliveira <nicarioster@gmail.com>
2019-02-18 14:17:10 -0300

    Remocao de itens na lista

--- introducao-javascript/js/form.js
+++ introducao-javascript/js/form.js
@@ -3,33 +3,29 @@ botaoAdicionar.addEventListener("click", function(event) {
     event.preventDefault();
 
     var form = document.querySelector("#form-adiciona");
+
     var paciente = obtemPacienteDoFormulario(form);
+
     var pacienteTr = montaTr(paciente);
 
     var erros = validaPaciente(paciente);
-    console.log(erros);
+
     if (erros.length > 0) {
         exibeMensagensDeErro(erros);
+
         return;
     }
 
     var tabela = document.querySelector("#tabela-pacientes");
+
     tabela.appendChild(pacienteTr);
+
     form.reset();
 
     var mensagensErro = document.querySelector("#mensagens-erro");
     mensagensErro.innerHTML = "";
+
 });
-
-function exibeMensagensDeErro(erros) {
-	var ul = document.querySelector("#mensagens-erro");
-	ul.innerHTML = "";
-
-	erros.forEach(function(erro)) {
-		var li = document.querySelector(#mensagens-erro);
-		li.textContent = erro;
-		ul.appendChild(li);
-	}
 
 function obtemPacienteDoFormulario(form) {
 
@@ -69,45 +65,40 @@ function validaPaciente(paciente) {
 
     var erros = [];
 
-    if (paciente.nome.length == 0){
+    if (paciente.nome.length == 0) {
         erros.push("O nome não pode ser em branco");
     }
 
-    if (paciente.gordura.length == 0){
+    if (paciente.gordura.length == 0) {
         erros.push("A gordura não pode ser em branco");
     }
 
-    if (paciente.peso.length == 0){
+    if (paciente.peso.length == 0) {
         erros.push("O peso não pode ser em branco");
     }
 
-    if (paciente.altura.length == 0){
+    if (paciente.altura.length == 0) {
         erros.push("A altura não pode ser em branco");
     }
 
-    if (!validaPeso(paciente.peso)){
+    if (!validaPeso(paciente.peso)) {
         erros.push("Peso é inválido");
     }
 
-    if (!validaAltura(paciente.altura)){
+    if (!validaAltura(paciente.altura)) {
         erros.push("Altura é inválida");
     }
 
     return erros;
 }
 
-function validaPeso(peso) {
-	if (peso >= 0 && peso <= 1000) {
-		return true;
-	} else {
-		return false;
-	}
-}
+function exibeMensagensDeErro(erros) {
+    var ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML = "";
 
-function validaAltura(altura) {
-	if (altura >= 0 && altura <= 3.0) {
-		return true;
-	}else {
-		return false;
-	}
+    erros.forEach(function(erro) {
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
 }
